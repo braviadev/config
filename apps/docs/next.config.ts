f2ba532242e5d3fbf27c892397c030a0eca94323
Olanrewaju Toyyib <olarewajutoyyib@gmail.com>
2026-04-19 23:10:54 +0100

    Update next.config.ts

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // DO NOT set output: 'export'
+}
 
-export default nextConfig;
+export default nextConfig
